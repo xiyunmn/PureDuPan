70d0fc4ab45d53409e3c93529797efe1e5f71ad9
xiyunmn <xiyunmn@users.noreply.github.com>
2026-07-17 15:47:55 +0800

    chore: bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val majorVersion = 1
-val minorVersion = 3
+val minorVersion = 4
 val patchVersion = 0
 
 val releaseVersionCode = majorVersion * 10000 + minorVersion * 100 + patchVersion
